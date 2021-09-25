d3f7180a4ffb4d9af2147c05cf6c53fae23ca2a8
McCray <tubach75@hotmail.com>
2021-09-25 13:06:30 -0400

    prelim config of index.js file

---
+++ rsw_client/src/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+//import rootReducer from "./reducers";
+
+const store = createStore(null, applyMiddleware(thunk));
+
+ReactDOM.render(
+  <Provider store={store}>
+    {/*<App />*/}
+  </Provider>,
+  document.getElementById("root")
+);
