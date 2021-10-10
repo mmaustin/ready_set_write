a1a2be8b6830619f72faa457a7cfcdd240945859
McCray <tubach75@hotmail.com>
2021-10-10 14:37:52 -0400

    added notes.js file for help remembering syntax

---
+++ Notes.js
@@ -0,0 +1,37 @@
+/*
+
+Stateless component
+
+const Name = () => {
+  return (
+    <div>
+      
+    </div>
+  );
+};
+
+
+//Component with state
+
+class ComponentName extends Component {
+
+  constructor(){
+    super();
+    this.state = {
+        array: [],
+    }
+  }
+
+  render(){
+      return (
+          <div>
+              
+          </div>
+      )
+  }
+
+}
+
+export default ComponentName;
+
+*/
